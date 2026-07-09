42ec6c14081add71285338a17bcd788a65fd1b5c
SajavaC <chung7748@gmail.com>
2026-07-09 12:34:33 -0500

    Update 02_Update_Oursales_modifiers.sql

--- 01_Data_Preparation/02_Update_Oursales_modifiers.sql
+++ 01_Data_Preparation/02_Update_Oursales_modifiers.sql
@@ -1,18 +1,16 @@
--- ==============================================================================
+-- =========================================================================================================================================
 -- Query Name:
 -- Update_Oursales_modifiers
 
 -- Purpose:
--- Updates StoreCode in the imported POS modifier sales table by matching store
--- names with the master Store table.
+-- Updates StoreCode in the imported POS modifier sales table by matching store names with the master Store table.
 
 -- Business Value:
--- Ensures modifier sales are assigned to the correct stores, allowing ingredient
--- consumption to be analyzed together with purchasing data.
+-- Ensures modifier sales are assigned to the correct stores, allowing ingredient consumption to be analyzed together with purchasing data.
 
 -- Used By:
 -- AllSales_Union
--- ==============================================================================
+-- =========================================================================================================================================
 
 UPDATE Oursales_modifiers
 INNER JOIN Store ON Oursales_modifiers.Location = Store.OurName
