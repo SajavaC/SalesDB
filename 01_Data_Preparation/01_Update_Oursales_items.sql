e9d72caf94b15756b00e718759c0b0c95ca49339
SajavaC <chung7748@gmail.com>
2026-07-10 14:26:44 -0500

    Update 01_Update_Oursales_items.sql

--- 01_Data_Preparation/01_Update_Oursales_items.sql
+++ 01_Data_Preparation/01_Update_Oursales_items.sql
@@ -9,7 +9,7 @@
 -- Standardizes store identifiers so retail sales data can be accurately compared with distributor's purchasing records during downstream analysis.
 
 -- Used By:
--- AllSales_Union.sql
+-- Oursales_items Table
 -- ==================================================================================================================================================
 
 UPDATE Oursales_items
