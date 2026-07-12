2ba1d049102e99700675e4093c4b245e5f7888b7
SajavaC <chung7748@gmail.com>
2026-07-11 19:23:32 -0500

    Create 01_y_AllStoreItem.sql

---
+++ 02_Analytics Engine/01_y_AllStoreItem.sql
@@ -0,0 +1,22 @@
+-- ===============================================================================================================================================================================
+-- Query Name:
+-- y_AllStoreItem
+
+-- Purpose:
+-- Generates every possible combination of active stores and inventory items.
+
+-- Business Value:
+-- Ensures stores with no purchasing activity are still included in downstream analysis, making it possible to identify missing orders and calculate ordering patterns accurately.
+
+-- Used By:
+-- Distributor_OrderCycle_ByItemStore.sql
+-- ===============================================================================================================================================================================
+
+SELECT
+    Store.StoreCode,
+    Store.DistributorName,
+    Item.ItemCode,
+    Item.ItemName
+FROM 
+    Store, 
+    Item;
