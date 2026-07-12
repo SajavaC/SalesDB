89bb85f003892d3d8c9796aad2598c64868ee284
SajavaC <chung7748@gmail.com>
2026-07-11 19:16:54 -0500

    Create 04_z_12ozcup_Template.sql

---
+++ 01_ETL & Data Preparation/04_z_12ozcup_Template.sql
@@ -0,0 +1,29 @@
+-- ================================================================================================================================
+-- Query Name:
+-- z_12ozcup_Template
+
+-- Purpose:
+-- Converts qualifying 12oz beverage sales into estimated 12oz cold cup consumption.
+
+-- Used By:
+-- OurSoldQty.sql
+-- ================================================================================================================================
+
+SELECT
+    Store.StoreCode AS StoreCode,
+    Store.City AS City,
+    Store.Province AS Province,
+    'Demo_Code_001' AS ItemCode,
+    Oursales_items.Month AS Month,
+    SUM(Oursales_items.[Units sold]) AS Qty
+FROM Oursales_items 
+INNER JOIN Store ON Store.StoreCode = Oursales_items.StoreCode
+WHERE
+    (Oursales_items.Item LIKE '*12 oz*' OR Oursales_items.Item LIKE '*12oz*')
+    AND Oursales_items.Category NOT LIKE '*Hot Drinks*'
+    AND Oursales_items.Category NOT LIKE '*OAKOFFEE*'
+GROUP BY
+    Store.StoreCode, 
+    Store.City, 
+    Store.Province, 
+    Oursales_items.Month;
