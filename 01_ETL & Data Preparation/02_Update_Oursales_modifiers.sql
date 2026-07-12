f1aea7da9449d855dba12a6fecbca00342cf6553
SajavaC <chung7748@gmail.com>
2026-07-11 19:14:58 -0500

    Create 02_Update_Oursales_modifiers.sql

---
+++ 01_ETL & Data Preparation/02_Update_Oursales_modifiers.sql
@@ -0,0 +1,17 @@
+-- =========================================================================================================================================
+-- Query Name:
+-- Update_Oursales_modifiers
+
+-- Purpose:
+-- Updates StoreCode in the imported POS modifier sales table by matching store names with the master Store table.
+
+-- Business Value:
+-- Ensures modifier sales are assigned to the correct stores, allowing ingredient consumption to be analyzed together with purchasing data.
+
+-- Used By:
+-- Oursales_modifiers Table
+-- =========================================================================================================================================
+
+UPDATE Oursales_modifiers
+INNER JOIN Store ON Oursales_modifiers.Location = Store.OurName
+SET Oursales_modifiers.StoreCode = Store.StoreCode;
